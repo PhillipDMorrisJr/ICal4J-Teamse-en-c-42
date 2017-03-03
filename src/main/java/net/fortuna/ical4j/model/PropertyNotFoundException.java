47f3531af7d5c1ffda99971287436dd5aff966fb
Phillip Morris <pmorris4@my.westga.edu>
2017-03-02 23:48:15 -0500

    Created PropertyNotFoundException

--- src/main/java/net/fortuna/ical4j/model/PropertyNotFoundException.java
+++ src/main/java/net/fortuna/ical4j/model/PropertyNotFoundException.java
@@ -2,4 +2,41 @@ package net.fortuna.ical4j.model;
 
 public class PropertyNotFoundException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor for PropertyNotFoundException
+	 */
+	public PropertyNotFoundException() {
+		super();
+	}
+
+	/**
+	 * Initialization constructor for PropertyNotFoundException that accepts a message
+	 * @param message reason for exception
+	 */
+	public PropertyNotFoundException(String message) {
+		super(message);
+	}
+
+	/**
+	 * Initialization constructor for PropertyNotFoundException that accepts a message and cause
+	 * @param message reason for exception
+	 * @param cause 
+	 */
+	public PropertyNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public PropertyNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public PropertyNotFoundException(Throwable cause){
+		super(cause);
+	}
 }
