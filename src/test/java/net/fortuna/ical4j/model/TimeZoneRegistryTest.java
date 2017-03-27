be8e0063d7d926cae2dc606fb866b45b74e8af0d
Jonathan Salmon <h4rdc0r3g4m3r360@yahoo.com>
2017-03-27 01:51:24 -0400

    Looks like the test for the timezoneRegistrationImpl are use in the DateTimeTest.java I Added a note to the created test case to look there!

--- src/test/java/net/fortuna/ical4j/model/TimeZoneRegistryTest.java
+++ src/test/java/net/fortuna/ical4j/model/TimeZoneRegistryTest.java
@@ -5,6 +5,10 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TimeZoneRegistryTest {
+	
+	// All of the test that would be here are all used in the Date Time test.
+	
+	
 
 	@Test
 	public void test() {
