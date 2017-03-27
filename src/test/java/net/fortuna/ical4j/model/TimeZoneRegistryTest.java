25f67066809220c6fb690a9f4ae351c0b0bc3af0
Jonathan Salmon <h4rdc0r3g4m3r360@yahoo.com>
2017-03-27 00:43:46 -0400

    Created a new TimeZoneRegistryTest.java class to build the test classes.

---
+++ src/test/java/net/fortuna/ical4j/model/TimeZoneRegistryTest.java
@@ -0,0 +1,14 @@
+package net.fortuna.ical4j.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TimeZoneRegistryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
