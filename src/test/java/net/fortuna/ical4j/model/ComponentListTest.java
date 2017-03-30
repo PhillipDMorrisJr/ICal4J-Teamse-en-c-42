725b46e98719f0ff8c2bd6ce0a0c3a88c391e092
Caleb Farara <cfarara1@my.westga.edu>
2017-03-29 23:08:46 -0400

    Working on ComponentList getComponents testing. ComponentListTest JUnit created.

---
+++ src/test/java/net/fortuna/ical4j/model/ComponentListTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package net.fortuna.ical4j.model;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit tests for <code>ComponentList</code> base class.
+ * 
+ * @author Caleb Farara
+ * @version Spring 2017
+ */
+public class ComponentListTest extends TestCase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
