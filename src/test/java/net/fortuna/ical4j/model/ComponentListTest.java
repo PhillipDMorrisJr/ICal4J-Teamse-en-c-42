3b3187112cf94d78f3e5fb83c5de7fc04f0f64e3
Caleb Farara <cfarara1@my.westga.edu>
2017-03-30 01:03:28 -0400

    Test 1: Test getComponents with an empty string as the name with some number of components in the list. getComponents(""), expected result should be a list that is empty COMPLETE!

--- src/test/java/net/fortuna/ical4j/model/ComponentListTest.java
+++ src/test/java/net/fortuna/ical4j/model/ComponentListTest.java
@@ -3,9 +3,13 @@
  */
 package net.fortuna.ical4j.model;
 
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import junit.framework.TestCase;
+import net.fortuna.ical4j.model.component.VEvent;
 
 /**
  * Unit tests for <code>ComponentList</code> base class.
@@ -14,10 +18,43 @@ import junit.framework.TestCase;
  * @version Spring 2017
  */
 public class ComponentListTest extends TestCase {
+	
+	private ComponentList<Component> componentList;
 
+	/**
+	 * Tests getComponents gets a list of components withthe emprty string as the name.
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetComponentsWithAnEmptyString() {
+		this.componentList = new ComponentList<Component>();
+		VEvent component1 = new VEvent();
+		this.componentList.add(component1);
+		assertEquals(new ComponentList<Component>(), componentList.getComponents(""));
+	}
+
+	/**
+	 * Tests getComponents gets a list of components with testDate1 string as the name.
+	 */
+	@Test
+	public void testGetComponentsWithASingleRecord() {
+		this.componentList = new ComponentList<Component>();		
+		Date componentDate = new Date(1000);
+		Dur componentDur = new Dur(0, 0, 0, 0);
+		Component component1 = new VEvent(componentDate, componentDur, "testDate1");
+		VEvent component2 = new VEvent();
+		this.componentList.add(component1);
+		this.componentList.add(component2);
+		assertEquals(component1, componentList.getComponents("VEvent"));
+	}
+	
+	@Test
+	public void testGetComponentsWithAThreeRecords() {
+		
+	}
+	
+	@Test
+	public void testGetComponentsWithAMultipleRecords() {
+		
 	}
 
 }
